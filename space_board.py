c65358ea31a9aa9ef8d195a2b8eedc32d28853e8
Ivan <ivan.felavel@gmail.com>
2018-10-06 20:55:08 -0500

    Dibuja recorrido

--- space_board.py
+++ space_board.py
@@ -9,12 +9,14 @@ class Space(object):
     meteors = [pygame.image.load('./sprites/Meteors/meteor'
                                  + str(i) + '.png') for i in range(1, 9)]
     ship = pygame.image.load('./sprites/blue_ship.png')
-    star_goal = pygame.image.load('./sprites/star_gold.png')
+    star_goal = pygame.image.load('./sprites/star.png')
     background = pygame.transform.scale(background, (grid_square_width, grid_square_height))
     meteors = [ pygame.transform.scale(meteor, (50, 50))\
                for meteor in meteors ]
-    trail = pygame.image.load('./sprites/star2.png')
-    trail = pygame.transform.scale(trail, (grid_square_width // 3, grid_square_height // 3))
+    path_vertical = pygame.image.load('./sprites/laserBlue12.png')
+    path_vertical = pygame.transform.scale(path_vertical, (grid_square_width // 10, grid_square_height // 2))
+    path_horizontal = pygame.transform.rotate(path_vertical, 90.0)
+    
 
     def __init__(self, grid_size, width=30, height=30):
         self.grid_size = grid_size
@@ -24,9 +26,13 @@ class Space(object):
                         for j in range(grid_size)]
         self.window_size = (Space.grid_square_width * grid_size,\
                             Space.grid_square_height * grid_size)
+        self.direction_path = [[[False for i in range(4)] for j in range(grid_size + 1)] for k in range(grid_size + 1)]
         self.ship = pygame.transform.scale(Space.ship, (Space.grid_square_width, Space.grid_square_height))
-
+        self.star_goal = pygame.transform.scale(
+            Space.star_goal, (Space.grid_square_width, Space.grid_square_height))
         self.ship_coordinates = None
+        self.goal_coordinates = None
+
 
     def generate_configuration(self):
         """
@@ -59,6 +65,8 @@ class Space(object):
         pygame.display.set_caption("Space Maze")
         done = False
         clock = pygame.time.Clock()
+        show_image = True
+        angle = 0.1
 
         while not done:
             for event in pygame.event.get():
@@ -67,32 +75,46 @@ class Space(object):
                 elif self.ship_coordinates and event.type == pygame.KEYDOWN:
                     self.visited[self.ship_coordinates[1] //
                                  Space.grid_square_height][self.ship_coordinates[0] // Space.grid_square_width] = True
+                    current_col = self.ship_coordinates[0] // Space.grid_square_width
+                    current_row = self.ship_coordinates[1] // Space.grid_square_height
                     if event.key == K_LEFT:
-                        # self.ship = pygame.transform.rotate(self.ship, 90.0)
+                        # me muevo a la izquierda
                         if self.ship_coordinates[0] - Space.grid_square_width >= 0\
                             and not self.grid[self.ship_coordinates[1] // Space.grid_square_height][(self.ship_coordinates[0] - Space.grid_square_width) // Space.grid_square_width]: 
-                            self.ship_coordinates[0] -= Space.grid_square_width 
+                            self.direction_path[current_row][current_col][3] = True
+                            self.direction_path[current_row][current_col - 1][1] = True
+                            self.ship_coordinates[0] -= Space.grid_square_width
+                            
                     elif event.key == K_UP:
                         if self.ship_coordinates[1] - Space.grid_square_height >= 0\
                                 and not self.grid[(self.ship_coordinates[1] - Space.grid_square_height) // Space.grid_square_height][self.ship_coordinates[0] // Space.grid_square_width]:
+                            self.direction_path[current_row][current_col][0] = True
+                            self.direction_path[current_row - 1][current_col][2] = True
                             self.ship_coordinates[1] -= Space.grid_square_height 
                     elif event.key == K_RIGHT:
                         # self.ship = pygame.transform.rotate(self.ship, -90.0)
                         if self.ship_coordinates[0] + Space.grid_square_width < self.grid_size * Space.grid_square_width\
                                 and not self.grid[self.ship_coordinates[1] // Space.grid_square_height][(self.ship_coordinates[0] + Space.grid_square_width) // Space.grid_square_width]:
+                            self.direction_path[current_row][current_col][1] = True
+                            self.direction_path[current_row][current_col + 1][3] = True
                             self.ship_coordinates[0] += Space.grid_square_width 
                     elif event.key == K_DOWN:
                         if self.ship_coordinates[1] + Space.grid_square_height < self.grid_size * Space.grid_square_height\
                                 and not self.grid[(self.ship_coordinates[1] + Space.grid_square_height) // Space.grid_square_height][self.ship_coordinates[0] // Space.grid_square_width]:
+                            self.direction_path[current_row][current_col][2] = True
+                            self.direction_path[current_row + 1][current_col][0] = True
                             self.ship_coordinates[1] += Space.grid_square_height
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
                     column = pos[0] // Space.grid_square_width
                     row = pos[1] // Space.grid_square_height
-                    if not self.ship_coordinates:
+                    if not self.ship_coordinates and not self.is_obstacle(row, column):
                         self.ship_coordinates = [Space.grid_square_width * column,\
                         Space.grid_square_height * row]
-                        print("Click ", pos, "Grid coordinates: ", row, column)
+                    elif self.ship_coordinates and not self.goal_coordinates and\
+                            not self.is_obstacle(row, column):
+                        self.goal_coordinates = [Space.grid_square_width * column,
+                                                 Space.grid_square_height * row]
             for row in range(self.grid_size):
                 for column in range(self.grid_size):
                     screen.blit(Space.background, (Space.grid_square_width * column,\
@@ -100,10 +122,25 @@ class Space(object):
                     if self.is_obstacle(row, column):
                         screen.blit(Space.meteors[self.grid[row][column]], (Space.grid_square_width * column,\
                                     Space.grid_square_height * row))
-                    if self.visited[row][column]:
-                        screen.blit(Space.trail, (Space.grid_square_width * column + Space.grid_square_width // 3,
-                                                  Space.grid_square_height * row + Space.grid_square_height // 3 ))
-            if self.ship_coordinates:
+                    semipaths = self.direction_path[row][column]
+                    if semipaths[0]:
+                        screen.blit(Space.path_vertical, (Space.grid_square_width * column + Space.grid_square_width // 2,
+                                                            Space.grid_square_height * row))
+                    if semipaths[1]:
+                        screen.blit(Space.path_horizontal, (Space.grid_square_width * column + Space.grid_square_width // 2,
+                                                            Space.grid_square_height * row + Space.grid_square_height // 2))
+                    if semipaths[2]:
+                        screen.blit(Space.path_vertical, (Space.grid_square_width * column + Space.grid_square_width // 2,
+                                                            Space.grid_square_height * row + Space.grid_square_height // 2))
+                    if semipaths[3]:
+                        screen.blit(Space.path_horizontal, (Space.grid_square_width * column,
+                                                            Space.grid_square_height * row + Space.grid_square_height // 2))
+            if self.goal_coordinates:
+                if self.goal_coordinates == self.ship_coordinates:
+                    self.star_goal = pygame.transform.rotate(self.star_goal, 90)
+                screen.blit(self.star_goal, self.goal_coordinates)
+                
+            if self.ship_coordinates and self.ship_coordinates != self.goal_coordinates:
                 screen.blit(self.ship, self.ship_coordinates)
             clock.tick(30)
             pygame.display.flip()
